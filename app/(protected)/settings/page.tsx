a253656d11c353c5d2f1f579abe928630795cec9
Ayush Gour <ayushgour03@gmail.com>
2024-06-02 23:38:21 +0530

    fix: callbacks added in auth

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,7 +1,23 @@
+import { auth, signOut } from '@/auth';
 import React from 'react';
 
-const Settings = () => {
-  return <div>Settings</div>;
+const Settings = async () => {
+  const session = await auth();
+
+  const formAction = async () => {
+    'use server';
+
+    await signOut();
+  };
+
+  return (
+    <div>
+      {JSON.stringify(session)}
+      <form action={formAction}>
+        <button type='submit'>Sign Out</button>
+      </form>
+    </div>
+  );
 };
 
 export default Settings;
--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import authConfig from '@/auth.config';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 import { db } from '@/lib/db';
+import { getUserById } from '@/lib/user';
 
 export const {
   handlers: { GET, POST },
@@ -10,6 +11,23 @@ export const {
   signOut,
   auth,
 } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
